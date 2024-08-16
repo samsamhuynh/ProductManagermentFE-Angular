da589f42349b35671bf9634b9fcc63775c06a7b1
samsamhuynh <uyenhuynh13122000@gmail.com>
2024-08-16 15:43:04 +0700

    create app.module

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { RouterOutlet } from "@angular/router";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, RouterOutlet],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
